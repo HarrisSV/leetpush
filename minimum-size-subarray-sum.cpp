6256c5b008963761d5b95268c9b053ee3e0b7ab5
Suyash Verma <suyashverma.6701@gmail.com>
2026-08-30 00:58:30 +0530

    Update solution: Minimum Size Subarray Sum

--- minimum-size-subarray-sum.cpp
+++ minimum-size-subarray-sum.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        int sum = 0, low = 0, high = 0;
+        int res = INT_MAX;
         int n = nums.size();
-        int left = 0;
-        int sum = 0;
-        int ans = INT_MAX;
+        int flag = 0;
 
-        for (int right = 0; right < n; right++) {
-            sum += nums[right];
-
-            while (sum >= target) {
-                int len = right - left + 1;
-                if (len < ans) {
-                    ans = len;
-                }
-                sum -= nums[left];
-                left++;
+        while(high < n)
+        {
+            sum += nums[high];
+            while(sum >= target)
+            {
+                int l = high - low + 1;
+                res = min(res, l);
+                sum = sum - nums[low];
+                low++;
+                flag = 1;
             }
+            high++;
         }
 
-        if (ans == INT_MAX) {
-            return 0;
+        if(flag == 1)
+        {
+            return res;
         }
-        return ans;
+       
+       else{
+           return 0;
+       }
+        
     }
 };
